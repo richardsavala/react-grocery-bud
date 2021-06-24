ac80f2a6c614e73b12c82370afc5e5f4c0db4156
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-24 01:32:31 -0600

    Add files via upload
    grocery bud - add items
    in App.js added edit and delete buttons
    in List.js added div 'grocery-list' which map items with variables id and title, then returns 'grocery-item' article with p 'title' and div 'btn-container' with the submit button for the form

--- src/App.js
+++ src/App.js
@@ -10,7 +10,16 @@ function App() {
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hell world");
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
   return (
     <section className="section-center">
@@ -30,10 +39,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
