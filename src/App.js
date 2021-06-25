558c3839b457059956f6daf5429307dd35654df6
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-25 16:38:24 -0600

    Add files via upload
    grocery bud - alert
    in Alert.js added useEffect 
    inside useEffect added variable timeout to clear alert after 3 seconds

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     e.preventDefault();
     if (!name) {
       // display alert
-      setAlert({ show: true, msg: "please enter value", type: "danger" });
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       // deal with edit
     } else {
@@ -26,10 +26,15 @@ function App() {
       setName("");
     }
   };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
