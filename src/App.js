1ffebf4486f17de68a9d4622f95f4d37bb9d1d47
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-26 00:27:04 -0600

    Add files via upload
    grocery bud - clear items
    in App.js added variable clearList
    added onClick to 'clear-btn' button to clear entire list

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     } else if (name && isEditing) {
       // deal with edit
     } else {
-      // show alert
+      showAlert(true, "success", "item added to list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -30,7 +30,10 @@ function App() {
   const showAlert = (show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
   };
-
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -52,7 +55,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
